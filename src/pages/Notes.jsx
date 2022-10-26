8de0b2ac29252f4aac9e1c093f70845242364aa6
Piyush Sarkar <iampiyushsarkar@gmail.com>
2022-10-26 22:24:35 +0530

    added Loading text

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -2,14 +2,19 @@ import { useEffect, useState } from 'react';
 import NoteCard from '../components/NoteCard';
 
 import Masonry from '@mui/lab/Masonry';
+import { Typography } from '@mui/material';
 
 const Notes = () => {
   const [notes, setNotes] = useState([]);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch(import.meta.env.VITE_API_SERVER)
       .then((res) => res.json())
-      .then((data) => setNotes(data.reverse()));
+      .then((data) => {
+        setNotes(data.reverse());
+        setIsPending(false);
+      });
   }, []);
 
   const handleDelete = async (id) => {
@@ -22,11 +27,18 @@ const Notes = () => {
   };
 
   return (
-    <Masonry columns={{ xs: 1, md: 2, lg: 3 }} spacing={3} sx={{ m: 0 }}>
-      {notes.map((note) => (
-        <NoteCard note={note} key={note.id} handleDelete={handleDelete} />
-      ))}
-    </Masonry>
+    <>
+      {isPending && (
+        <Typography variant="h5" color="textSecondary">
+          Loading...
+        </Typography>
+      )}
+      <Masonry columns={{ xs: 1, md: 2, lg: 3 }} spacing={3} sx={{ m: 0 }}>
+        {notes.map((note) => (
+          <NoteCard note={note} key={note.id} handleDelete={handleDelete} />
+        ))}
+      </Masonry>
+    </>
   );
 };
 
